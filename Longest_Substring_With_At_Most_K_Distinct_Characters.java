d6e86ac4571e79df6881c268b566695ea137a13c
pt657407064 <taopan1211@gmail.com>
2018-02-17 23:51:23 -0500

    LC 340

---
+++ Longest_Substring_With_At_Most_K_Distinct_Characters.java
@@ -0,0 +1,16 @@
+//Time O(N) space O(1)
+class Solution {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        int[] i = new int[256];
+        int max = 0,count = 0;
+        for(int st = 0,e = 0;e<s.length();e++){
+            if(i[s.charAt(e)]++ == 0) count ++; //This means there a new character come in
+            if(count >= k) {
+                while(--i[s.charAt(st++)] > 0);
+                count--;
+            }
+            max = Math.max(e-st+1,max);
+        }
+        return max;
+    }
+}
