9730ba7d1d4a0a03e6c3213a0cf0c69d5ce9fa5e
pt657407064 <taopan1211@gmail.com>
2018-02-21 13:12:40 -0500

    LC 235

---
+++ Lowest_Common_Ancestor_Of_A_Binary_Search_Tree.java
@@ -0,0 +1,7 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(p.val > root.val && q.val > root.val) return lowestCommonAncestor(root.right,p,q);
+        if(q.val < root.val && p.val < root.val) return lowestCommonAncestor(root.left,p,q);
+        return root;
+    }
+}
