1b31ba3dd1d9c1588e08a05a2541f2d0dca0e95b
pt657407064 <taopan1211@gmail.com>
2018-02-22 02:15:28 -0500

    LC 62

--- Unique_Paths.java
+++ Unique_Paths.java
@@ -15,14 +15,14 @@ class Solution {
 //Time O(M*N) space O(N);
 class Solution{
   public int uniquePaths(int m, int n) {
-      int[] dp = new int[m];
-      Arrays.fill(row,1);
+      int[] dp = new int[n];
+      Arrays.fill(dp,1);
       for(int i = 1;i<m;i++){
           for(int j = 1;j<n;j++){
             //Store last row value in an array, and plus the previous col number which has been added up prviously
-              dp[j] = dp[j-1];
+              dp[j] += dp[j-1];
           }
       }
-      return dp[m-1];
+      return dp[n-1];
   }
 }
