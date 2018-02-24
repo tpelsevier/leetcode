b132fa546471a536d3d00015b040aed595d115b9
pt657407064 <taopan1211@gmail.com>
2018-02-23 21:24:41 -0500

    LC 96

---
+++ Unique_Binary_Search_Trees.java
@@ -0,0 +1,14 @@
+//Time O(N!) space O(N)
+class Solution {
+    public int numTrees(int n) {
+        if(n<2) return 1;
+        int[] dp = new int[n+1];
+        dp[0]=dp[1] = 1;
+        for(int i = 2;i<=n;i++){
+            for(int j = 0; j < i;j++){
+                dp[i] += dp[j]*dp[i-j-1];
+            }
+        }
+        return dp[n];
+    }
+}
