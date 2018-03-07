497b04eb139bdd9ead3c41e12db92a39c13d1e9e
pt657407064 <taopan1211@gmail.com>
2018-03-07 00:05:46 -0500

    LC 216

---
+++ Combination_Sum_III.java
@@ -0,0 +1,21 @@
+//Time O(N) Space O(N)
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        helper(new ArrayList<>(),n,k,1);
+        return res;
+    }
+    private void helper(List<Integer> ls,int n,int k,int start){
+        if(ls.size() == k){
+            if(n == 0) {
+                res.add(new ArrayList<>(ls));
+            }
+            return;
+        }
+        for(int i = start;i<=9;i++){
+            ls.add(i);
+            helper(ls,n-i,k,i+1);
+            ls.remove(ls.size()-1);
+        }
+    }
+}
