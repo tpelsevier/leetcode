1b702e9bc88f994d0d0d4e61664f9f85c8911414
pt657407064 <taopan1211@gmail.com>
2018-03-06 23:09:22 -0500

    LC 39

---
+++ Combination_Sum.java
@@ -0,0 +1,21 @@
+//Time O(N)  Space O(N)
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates , int target) {
+        Arrays.sort(candidates);
+        helper(candidates,target,0,new ArrayList<>());
+        return res;
+    }
+    public void helper(int[] c, int remain, int start, List<Integer> ls){
+        if(remain < 0 ) return;
+        if(remain == 0) {
+            res.add(new ArrayList<>(ls));
+            return;
+        }
+        for(int i = start;i<c.length;i++){
+            ls.add(c[i]);
+            helper(c,remain-c[i],i,ls);
+            ls.remove(ls.size()-1);
+        }
+    }
+}
