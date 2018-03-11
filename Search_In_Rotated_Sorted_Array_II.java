ea8b999c457d0eaed9abe199883cece7d27b4fe1
pt657407064 <taopan1211@gmail.com>
2018-03-11 17:57:03 -0400

    LC 81

---
+++ Search_In_Rotated_Sorted_Array_II.java
@@ -0,0 +1,27 @@
+//Time O(LogN) Space O(N)
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int l = 0, r = nums.length-1;
+        while(l <= r){
+            int mid = (l+r) / 2;
+            if(nums[mid] == target|| nums[r] == target || nums[l] == target) return true;
+            // Make sure this is a regular sorted order
+            if(nums[mid] < nums[r]){
+                //if target is greater thahn mid and smaller than r edge
+                //then it located between mid and r
+                if(target > nums[mid] && target < nums[r]) l = mid+1;
+                //Else it will be located between l and mid
+                else r = mid -1;
+                //This is not regular sorted order
+            }else if(nums[mid] > nums[r]){
+                //if target greater than l edge and smaller than mid point, then it located between l and mid
+                if(target > nums[l] && target < nums[mid]) r = mid -1;
+                //else it located between mid and r
+                else l = mid +1;
+            }
+            //else should minus 
+            else r--;
+        }
+        return false;
+    }
+}
