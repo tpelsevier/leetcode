f5ac327c977552f31675b095c521bdf3c82a4036
pt657407064 <taopan1211@gmail.com>
2018-03-15 16:13:52 -0400

    LC 323

--- Number_Of_Connected_Components_In_An_Undirected_Graph.java
+++ Number_Of_Connected_Components_In_An_Undirected_Graph.java
@@ -28,3 +28,26 @@ class Solution {
         return set.size();
     }
 }
+//Time O(LogN) Space O(N)
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        int [] roots = new int[n];
+        for(int i=0;i<n;i++) roots[i] = i;
+        for(int[] e: edges){
+            int p = findRoot(roots,e[0]);
+            int q = findRoot(roots,e[1]);
+            if(p != q){
+                roots[p] = q;
+                n--;
+            }
+        }
+        return n;
+    }
+    private int findRoot(int[] root, int id){
+        while(id != root[id]){
+            root[id] = root[root[id]];
+            id = root[id];
+        }
+        return id;
+    }
+}
