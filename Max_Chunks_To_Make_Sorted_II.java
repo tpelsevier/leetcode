ac20eda7cc05def8623cc6f7ca7ef71797f86f0d
pt657407064 <taopan1211@gmail.com>
2018-03-29 19:28:04 -0400

    LC 768

---
+++ Max_Chunks_To_Make_Sorted_II.java
@@ -0,0 +1,29 @@
+/*
+           [2,1,3,4,4]
+maxLeft =  [2,2,3,4,4]
+minRight = [1,1,3,4,4]
+if maxLeft[i] <= minRight means that everything before i is not sorted,
+since maxLeft is smaller than minRight, there will be a new piece of chunk
+*/
+
+//Time O(N) Space O(N)
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int n = arr.length;
+        int[] maxLeft = new int[n];
+        int[] minRight = new int[n];
+        maxLeft[0] = arr[0];
+        for(int i = 1,j = n-2; i < n && j >= 0;j--,i++){
+            maxLeft[i] = Math.max(maxLeft[i-1],arr[i]);
+        }
+        minRight[n-1] = arr[n-1];
+        for(int i = n-2; i >= 0;i--){
+            minRight[i] = Math.min(minRight[i+1],arr[i]);
+        }
+        int count =0;
+        for(int i = 0;i < n-1; i++){
+            if(maxLeft[i] <= minRight[i+1]) count++;
+        }
+        return count+1;
+    }
+}
