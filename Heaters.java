6d1e148f4b5f3299f9a4c9170a90910de303a60c
pt657407064 <taopan1211@gmail.com>
2018-04-10 22:29:44 -0400

    475

--- Heaters.java
+++ Heaters.java
@@ -1,4 +1,4 @@
-//Time max(O(nlogn), O(mlogn)) Space O(1) 
+//Time max(O(nlogn), O(mlogn)) Space O(1)
 class Solution {
     public int findRadius(int[] houses, int[] heaters) {
         Arrays.sort(heaters);
@@ -7,6 +7,7 @@ class Solution {
             int index = Arrays.binarySearch(heaters,house);
             if(index < 0){
                 index = ~index;
+                //Find the house between two heaters
                 int d1 = index - 1 >= 0 ?house-heaters[index-1]:Integer.MAX_VALUE;
                 int d2 = index < heaters.length? heaters[index] - house:Integer.MAX_VALUE;
                 res = Math.max(res,Math.min(d1,d2));
