65430befaab0bbdbd61e654d3c48e69129c4e9de
pt657407064 <taopan1211@gmail.com>
2018-04-13 01:58:14 -0400

    LC 207

---
+++ Course_Schedule.java
@@ -0,0 +1,25 @@
+//Time O(V+E)
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] idr = new int[numCourses];
+        for(int[] pres: prerequisites){
+            idr[pres[1]]++;
+        }
+        Queue<Integer> queue = new ArrayDeque<>();
+        for(int i = 0; i < idr.length;i++){
+            if(idr[i] == 0) queue.offer(i); //Find how many prerequisites the course has 
+        }
+        while(!queue.isEmpty()){
+            int pre = queue.poll();
+            numCourses--;
+            for(int[] p: prerequisites){
+                if(p[0] == pre) {
+                    idr[p[1]]--;
+                    if(idr[p[1]] == 0)
+                    queue.offer(p[1]);
+                }
+            }
+        }
+        return numCourses <= 0;
+    }
+}
