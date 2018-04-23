e797a76df6c8c1c0f97792d73283b78df5acba99
pt657407064 <taopan1211@gmail.com>
2018-04-23 02:52:44 -0400

    LC 822

--- Card_Flipping_Game.java
+++ Card_Flipping_Game.java
@@ -1,8 +1,14 @@
+/*
+This question means if two cards are same number in both back and front
+it will not be the smallest value no matter how we flip it
+*/
+
 //Time O(N) Space O(N)
 class Solution {
     public int flipgame(int[] fronts, int[] backs) {
         HashSet<Integer> same = new HashSet<>();
         for(int i = 0; i< fronts.length;i++) if(fronts[i] == backs[i]) same.add(fronts[i]);
+        if(same.size() == fronts.length) return 0;
         int res = 3000;
         for(int f: fronts) if(!same.contains(f)) res = Math.min(res,f);
         for(int b: backs) if(!same.contains(b)) res = Math.min(res,b);
