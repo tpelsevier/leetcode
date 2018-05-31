65a90eb24ea220d256da3caeb064c69c9625c433
pt657407064 <taopan1211@gmail.com>
2018-05-31 15:39:35 -0400

    LC 515

---
+++ Find_Largest_Value_In_Each_Tree_Row.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ //Time O(N) Space (log2(N))
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if(root == null) return ans;
+        findLargest(root,0,ans);
+        return ans;
+    }
+    public void findLargest(TreeNode root,int level,List<Integer> ans){
+        if(ans.size() <= level) ans.add(root.val);
+        else ans.set(level,Math.max(root.val,ans.get(level)));
+        if(root.left != null) findLargest(root.left,level+1,ans);
+        if(root.right != null) findLargest(root.right,level+1,ans);
+    }
+}
