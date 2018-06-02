bea2da9bfc6221a37fe658a20fe39cfb74bddb34
pt657407064 <taopan1211@gmail.com>
2018-06-02 15:07:55 -0400

    LC 643

--- Maximum_Average_Subarray_I.java
+++ Maximum_Average_Subarray_I.java
@@ -2,11 +2,11 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
         double max = Integer.MIN_VALUE;
-        int total = 0;
+        double total = 0;
         for(int i = 0,j = 0; i < nums.length;i++){
             total += nums[i];
             if(i >= k-1) {
-                max = Math.max((double)total/k,max);
+                max = Math.max(total/k,max);
                 total-=nums[j++];
             }
 
