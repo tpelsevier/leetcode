59e8f4c0a88c6fd8f53afdf2c5a124024aaa74d1
pt657407064 <taopan1211@gmail.com>
2018-06-05 03:22:47 -0400

    LC 667

---
+++ Beautiful_Arrangement_II.java
@@ -0,0 +1,10 @@
+//Time O(N) Space O(1)
+class Solution {
+    public int[] constructArray(int n, int k) {
+        int[] res = new int[n];
+        for(int i = 0,l = 1, r = n;i < n;i++){
+            res[i] = k > 1?(k--%2 != 0? l++:r--):l++;
+        }
+        return res;
+    }
+}
