60d266a1377eb1a9dd475a93cca41ce1fb6fc4ea
pt657407064 <taopan1211@gmail.com>
2018-06-17 03:14:58 -0400

    LC 854

--- K-Similar_Strings.java
+++ K-Similar_Strings.java
@@ -1,15 +1,21 @@
 //Time O(N) Space O(N)
 class Solution {
+  //Overall idea is to make the string at the begining the same
+  //Then start swapping rest of the arrays
     public int kSimilarity(String A, String B) {
         char[] a = A.toCharArray();
         char[] b = B.toCharArray();
         return helper(a,b,0,0);
     }
     private int helper(char[] now, char[] target, int start, int count){
+        //If the start is greater than now length
         if(start >= now.length) return count;
+        //Continue to next index if they are equal
         if(now[start] == target[start]){
             return helper(now,target,start+1,count);
         }
+        //If there is no match element at end
+        //It only returns the MAX_VALUE;
         int min = Integer.MAX_VALUE;
         for(int i = start+1; i < now.length;i++){
             if(now[i] == target[i] || now[i] != target[start]) continue;
