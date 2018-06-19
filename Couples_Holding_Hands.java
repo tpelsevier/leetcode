4bb9c7c006bf33b790ed9ef70e4693bd6884cdba
pt657407064 <taopan1211@gmail.com>
2018-06-18 22:47:17 -0400

    LC 765

--- Couples_Holding_Hands.java
+++ Couples_Holding_Hands.java
@@ -11,7 +11,7 @@ class Solution {
         for(int i = 0; i < n;i+=2){
             int x = row[i];
             int half = x^1; // (0 1) (2 3)
-             if(row[i+1] == half) continue;
+            if(row[i+1] == half) continue;
             count++;
             int ind = pos[half]; //Find the index of the half of row i
             pos[row[i+1]] = ind; //Set the index of current value of at i+1 to the pos of the half
