861b4daaca5a878931fde5f3f7879f17713003a2
pt657407064 <taopan1211@gmail.com>
2018-06-21 23:31:59 -0400

    LC 330

--- Patching_Array.java
+++ Patching_Array.java
@@ -4,11 +4,12 @@ class Solution {
         long miss = 1;
         int patches = 0, i = 0;
         while(miss <= n){
+            //See if missing number is at this stage cover the smallest number
             if(i < nums.length && nums[i] <= miss){
                 miss+=nums[i++];
             }else{
-                miss *= 2;
-                patches++;
+                miss *= 2; //If no missing number should be add itself
+                patches++; //Then we have a patches increase
             }
         }
         return patches;
