edc90c6ad5574a3ee091321aa5f16ab897656c45
pt657407064 <taopan1211@gmail.com>
2018-06-28 11:41:30 -0400

    LC 759

---
+++ Employee_Free_Time.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+ //Time O(NlogN) Space O(N)
+class Solution {
+    public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+        List<Integer> starts = new ArrayList<>();
+        List<Integer> ends = new ArrayList<>();
+        for(List<Interval> ls: schedule){
+            for(Interval interval: ls){
+                starts.add(interval.start);
+                ends.add(interval.end);
+            }
+        }
+        Collections.sort(starts);
+        Collections.sort(ends);
+        List<int[]> merge = new ArrayList<>();
+        int size = starts.size();
+        for(int i = 0, j = 0; i < size;i++){
+            if(i == size-1 || ends.get(i) < starts.get(i+1)){
+                merge.add(new int[]{starts.get(j),ends.get(i)});
+                j = i+1;
+            }
+        }
+
+        List<Interval> res = new ArrayList<>();
+        for(int i = 1; i < merge.size();i++){
+            int s = merge.get(i-1)[1], e = merge.get(i)[0];
+            res.add(new Interval(s,e));
+        }
+
+        return res;
+    }
+}
